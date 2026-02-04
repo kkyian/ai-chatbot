3b84cd6ed2229d0af94dbf35ace092b25f675ea2
kkyian <kkyian@icloud.com>
2026-02-05 07:04:00 +0800

    made main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+what_to_say = {'hello':'hello', 'bye':'bye'}
+while True:
+    input1 = input('What do you want to say:')
+    if input1 in what_to_say:
+        print(what_to_say[input1])
+    else:
+        say = input(f'What would you say to reply {input1}:')
+        what_to_say[input1] = say
+        print(what_to_say[input1])
